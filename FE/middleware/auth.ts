c6cc0195ffd10f75891b663793c32bfe5ef89b36
trinhlk <trinhlk@sankei-bc.co.jp>
2025-03-03 09:43:51 +0700

    fix conflict

--- FE/middleware/auth.ts
+++ FE/middleware/auth.ts
@@ -3,11 +3,6 @@ import { useAuthStore } from '../stores/auth'
 
 export default defineNuxtRouteMiddleware(() => {
   const authStore = useAuthStore()
-<<<<<<< HEAD
-=======
-
-  // Nếu không có token, chuyển hướng về trang đăng nhập
->>>>>>> e43d149f81e6b26604a2804bfe55d739b629db76
   if (!authStore.token) {
     return navigateTo('/authentication/login')
   }
--- FE/stores/auth.ts
+++ FE/stores/auth.ts
@@ -3,7 +3,6 @@ import { useCookie } from 'nuxt/app'
 
 export const useAuthStore = defineStore('auth', {
   state: () => ({
-<<<<<<< HEAD
     token: useCookie<string | null>('token', { default: () => null }),
     user: useCookie<any | null>('user', { default: () => null })
   }),
@@ -28,24 +27,8 @@ export const useAuthStore = defineStore('auth', {
       this.token = null;
       this.user = null;
 
-=======
-    token: useCookie<string | null | any>('token', { default: () => null }),
-    user: useCookie<any | null>('user', { default: () => null })
-  }),
-
-  actions: {
-    setAuthData(token: string, user: any) {
-    console.log("api trả về :");
-
-      this.token = token
-      this.user = user
-    },
-
-    logout() {
-      this.token = null
-      this.user = null
->>>>>>> e43d149f81e6b26604a2804bfe55d739b629db76
-      localStorage.removeItem('rememberMe');
+      localStorage.removeItem('jwt');
+      localStorage.removeItem('user');
     }
   }
 })
